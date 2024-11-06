3c5ac1a9f7ffe7b7cf71dad0f79131d046b35e80
for { kys() } <yyeet62425@gmail.com>
2024-11-06 20:28:35 +0530

    copy dot sh

---
+++ copy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+DIRS_TO_COPY="alacritty betterlockscreen bspwm btop dunst fish nvim picom polybar sxhkd"
+
+SOURCE_DIR="$HOME/.config"
+
+DEST_DIR="$(pwd)/cfg"
+
+for dir in $DIRS_TO_COPY; do
+  src_dir="$SOURCE_DIR/$dir"
+  
+  if [ -d "$src_dir" ]; then
+    cp -r "$src_dir" "$DEST_DIR"
+    echo "Copied $src_dir to $DEST_DIR"
+  else
+    echo "Directory $src_dir does not exist in $SOURCE_DIR"
+  fi
+done
