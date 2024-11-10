b4d912778f65b818bde97c0a00430f327d7f1d19
for { kys() } <yyeet62425@gmail.com>
2024-11-10 18:14:43 +0530

    GHOSTTY!!!

--- cfg/fish/config.fish
+++ cfg/fish/config.fish
@@ -1,3 +1,7 @@
+export _JAVA_AWT_WM_NONREPARENTING=1
+
+set -gx PATH /opt/cuda/bin $PATH
+set -g LD_LIBRARY_PATH /opt/cuda/lib64 $LD_LIBRARY_PATH
+
 if status is-interactive
-	export _JAVA_AWT_WM_NONREPARENTING=1
 end
