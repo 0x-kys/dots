1e0b7bf53a16d0864211c40e57bf0930eca32507
for { kys() } <yyeet62425@gmail.com>
2024-11-13 22:04:46 +0530

    window focus

--- cfg/fish/config.fish
+++ cfg/fish/config.fish
@@ -6,3 +6,10 @@ set -g LD_LIBRARY_PATH /opt/cuda/lib64 $LD_LIBRARY_PATH
 if status is-interactive
   alias todo="nvim ~/.stuff.md"
 end
+
+# pnpm
+set -gx PNPM_HOME "/home/syk/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
