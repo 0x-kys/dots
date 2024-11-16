73bc683ca6ee0d5e90951c2fdf06cfe48da9de0d
for { kys() } <yyeet62425@gmail.com>
2024-11-17 00:43:12 +0530

    updates

--- copy.sh
+++ copy.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-DIRS_TO_COPY="alacritty betterlockscreen bspwm btop dunst fish ghostty nvim picom polybar sxhkd"
-
+DIRS_TO_COPY="alacritty betterlockscreen bspwm btop dunst fish ghostty nvim picom polybar sxhkd zed"
 SOURCE_DIR="$HOME/.config"
-
 DEST_DIR="$(pwd)/cfg"
 
 for dir in $DIRS_TO_COPY; do
@@ -19,4 +17,15 @@ done
 
 echo "Copying scripts"
 cp -r ~/scripts .
+
+# Remove the API key from the Zed configuration
+ZED_CONFIG="$DEST_DIR/zed/settings.json"
+
+if [ -f "$ZED_CONFIG" ]; then
+  # Remove comments and delete the API key using jq
+  sed '/\/\//d' "$ZED_CONFIG" | jq 'del(.lsp.wakatime.settings."api-key")' > "$ZED_CONFIG.tmp" && mv "$ZED_CONFIG.tmp" "$ZED_CONFIG"
+  echo "Removed API key from Zed config"
+else
+  echo "Zed config file not found in $ZED_CONFIG"
+fi
 
