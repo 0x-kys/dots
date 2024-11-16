7dc4d35dd4584cb6dbf54075cac5c8dfe1f3c874
for { kys() } <yyeet62425@gmail.com>
2024-11-17 00:43:12 +0530

    updates

--- cfg/fish/config.fish
+++ cfg/fish/config.fish
@@ -1,6 +1,7 @@
 export _JAVA_AWT_WM_NONREPARENTING=1
 
 set -gx PATH /opt/cuda/bin $PATH
+set -gx PATH $HOME/go/bin $PATH
 set -g LD_LIBRARY_PATH /opt/cuda/lib64 $LD_LIBRARY_PATH
 
 if status is-interactive
@@ -13,3 +14,7 @@ if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
