6c91ed271f34fa5e82920f32056520c8d260ac7f
for { kys() } <yyeet62425@gmail.com>
2025-01-03 10:54:28 +0530

    formatted home dot nix

--- cfg/nixcfg/home.nix
+++ cfg/nixcfg/home.nix
@@ -1,144 +1,164 @@
 { config, pkgs, ... }:
 
 let
-  nvimPath = "${config.home.homeDirectory}/.config/nixcfg/nvim";
-  ghosttyPath = "${config.home.homeDirectory}/.config/nixcfg/ghostty";
-  fishPath = "${config.home.homeDirectory}/.config/nixcfg/fish";
-  glowPath = "${config.home.homeDirectory}/.config/nixcfg/glow";
-  hyprPath = "${config.home.homeDirectory}/.config/nixcfg/hypr";
-  waybarPath = "${config.home.homeDirectory}/.config/nixcfg/waybar";
-  scriptsPath = "${config.home.homeDirectory}/.config/nixcfg/scripts";
-  btopPath = "${config.home.homeDirectory}/.config/nixcfg/btop";
+  configPaths = [
+    {
+      name = "nvim";
+      path = "${config.home.homeDirectory}/.config/nixcfg/nvim";
+    }
+    {
+      name = "ghostty";
+      path = "${config.home.homeDirectory}/.config/nixcfg/ghostty";
+    }
+    {
+      name = "fish";
+      path = "${config.home.homeDirectory}/.config/nixcfg/fish";
+    }
+    {
+      name = "glow";
+      path = "${config.home.homeDirectory}/.config/nixcfg/glow";
+    }
+    {
+      name = "hypr";
+      path = "${config.home.homeDirectory}/.config/nixcfg/hypr";
+    }
+    {
+      name = "waybar";
+      path = "${config.home.homeDirectory}/.config/nixcfg/waybar";
+    }
+    {
+      name = "scripts";
+      path = "${config.home.homeDirectory}/.config/nixcfg/scripts";
+    }
+    {
+      name = "btop";
+      path = "${config.home.homeDirectory}/.config/nixcfg/btop";
+    }
+  ];
 in {
-  home.username = "syk";
-  home.homeDirectory = "/home/syk";
+  home = {
+    username = "syk";
+    homeDirectory = "/home/syk";
 
-  xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink nvimPath;
-  xdg.configFile."ghostty".source =
-    config.lib.file.mkOutOfStoreSymlink ghosttyPath;
-  xdg.configFile."fish".source = config.lib.file.mkOutOfStoreSymlink fishPath;
-  xdg.configFile."glow".source = config.lib.file.mkOutOfStoreSymlink glowPath;
-  xdg.configFile."hypr".source = config.lib.file.mkOutOfStoreSymlink hyprPath;
-  xdg.configFile."waybar".source =
-    config.lib.file.mkOutOfStoreSymlink waybarPath;
-  xdg.configFile."scripts".source =
-    config.lib.file.mkOutOfStoreSymlink scriptsPath;
-  xdg.configFile."btop".source = config.lib.file.mkOutOfStoreSymlink btopPath;
-
-  home.file = {
-    ".icons/WhiteSur-cursors" = {
-      source = "${pkgs.whitesur-cursors}/share/icons/WhiteSur-cursors";
-      recursive = true;
+    file = {
+      ".icons/WhiteSur-cursors" = {
+        source = "${pkgs.whitesur-cursors}/share/icons/WhiteSur-cursors";
+        recursive = true;
+      };
     };
+
+    packages = with pkgs; [
+      brave
+      neovim
+      spotify
+      ghostty
+      libreoffice
+
+      fastfetch
+      tmux
+      nnn
+
+      zip
+      xz
+      unzip
+      p7zip
+
+      ripgrep
+      jq
+      yq-go
+      eza
+      fzf
+
+      mtr
+      iperf3
+      dnsutils
+      ldns
+      aria2
+      socat
+      nmap
+      ipcalc
+
+      cowsay
+      fortune
+      file
+      which
+      tree
+      gnused
+      gnutar
+      gawk
+      zstd
+      gnupg
+
+      nix-output-monitor
+
+      glow
+      bat
+      tmux
+      tmuxPlugins.sensible
+      tmuxPlugins.vim-tmux-navigator
+
+      btop
+      iotop
+      iftop
+
+      strace
+      ltrace
+      lsof
+
+      sysstat
+      lm_sensors
+      ethtool
+      pciutils
+      usbutils
+
+      go
+      zig
+      bun
+      cmake
+      clang
+      rustup
+      nodejs
+      python3
+      corepack
+
+      obs-studio
+      obs-studio-plugins.wlrobs
+      obs-studio-plugins.obs-pipewire-audio-capture
+
+      zls
+      gopls
+      pyright
+      marksman
+      lua-language-server
+      golangci-lint-langserver
+      vscode-langservers-extracted
+      typescript-language-server
+      tailwindcss-language-server
+      svelte-language-server
+      markdownlint-cli
+      nixfmt-classic
+      nixd
+
+      grim
+      slurp
+
+      whitesur-cursors
+      nwg-look
+    ];
   };
+
+  xdg.configFile = builtins.listToAttrs (map (c: {
+    name = c.name;
+    value = { source = config.lib.file.mkOutOfStoreSymlink c.path; };
+  }) configPaths);
 
   xresources.properties = {
     "Xcursor.size" = 24;
     "Xft.dpi" = 96;
   };
 
-  home.packages = with pkgs; [
-    brave
-    neovim
-    spotify
-    ghostty
-    libreoffice
-
-    fastfetch
-    tmux
-    nnn
-
-    zip
-    xz
-    unzip
-    p7zip
-
-    ripgrep
-    jq
-    yq-go
-    eza
-    fzf
-
-    mtr
-    iperf3
-    dnsutils
-    ldns
-    aria2
-    socat
-    nmap
-    ipcalc
-
-    cowsay
-    fortune
-    file
-    which
-    tree
-    gnused
-    gnutar
-    gawk
-    zstd
-    gnupg
-
-    nix-output-monitor
-
-    glow
-    bat
-    tmux
-    tmuxPlugins.sensible
-    tmuxPlugins.vim-tmux-navigator
-
-    btop
-    iotop
-    iftop
-
-    strace
-    ltrace
-    lsof
-
-    sysstat
-    lm_sensors
-    ethtool
-    pciutils
-    usbutils
-
-    go
-    zig
-    bun
-    cmake
-    clang
-    rustup
-    nodejs
-    python3
-    corepack
-
-    obs-studio
-    obs-studio-plugins.wlrobs
-    obs-studio-plugins.obs-pipewire-audio-capture
-
-    zls
-    gopls
-    pyright
-    marksman
-    lua-language-server
-    golangci-lint-langserver
-    vscode-langservers-extracted
-    typescript-language-server
-    tailwindcss-language-server
-    svelte-language-server
-    markdownlint-cli
-    nixfmt-classic
-    nixd
-
-    grim
-    slurp
-
-    whitesur-cursors
-    nwg-look
-  ];
-
   programs = {
     git = { enable = true; };
-
     home-manager = { enable = true; };
   };
 
