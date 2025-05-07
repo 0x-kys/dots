dedb2c14ba07d82f79730ac19a8be2fd3eb95e62
for { kys() } <yyeet62425@gmail.com>
2025-05-07 10:43:07 +0530

    waybar && mako

--- copy.sh
+++ copy.sh
@@ -1,1 +1,14 @@
 #!/usr/bin/env sh
+
+DOTFILES="waybar wofi mako hypr i3 rofi foot starship.toml alacritty"
+
+for item in $DOTFILES; do
+  if [ -e "$HOME/.config/$item" ]; then
+    cp -r "$HOME/.config/$item" .
+    echo "Copied/Updated $item to $(pwd)"
+  else
+    echo "Warning: $item not found in ~/.config"
+  fi
+done
+
+echo "Dotfiles copied to $(pwd)"
